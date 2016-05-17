6ff16cd13a5639682bc44e0817128b3ec8cbcb9c
Trent Whitinger <twhitinger@gmail.com>
2016-05-17 17:18:11 -0600

    making progress I guess

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,36 +1,78 @@
 require_relative "node"
 
 class LinkedList
-  attr_reader :head
+  attr_reader :head, :remaining_data, :current
   def initialize(head = nil)
     @head = head
     @count = 0
+    @current = @head
+
   end
-  def append(node)
-    if @count == 0
-      @head = node
-      @count += 1
-    elsif @count == 1
-      @head.next_node = Node.new("deep")
-      @count += 1
-      # check if head has a next node, if nil
-      # @head.next_node to a new instantiated node
+  def head_create(data)
+    data = data.split[0]
+    node = Node.new(data)
+    @head = node
+    @count += 1
+  end
+  def add_node_not_head(data)
+    data = data.split[0]
+
+    # go to the last node and add it to next node
+    @current = @head
+    until @current.next_node == nil
+      @current = @current.next_node
+    end
+    @current.next_node = Node.new(data)
+  end
+
+  def append(data)
+    if @head == nil
+      head_create(data)
+    else
+      add_node_not_head(data)
+    end
+
+
+
+    # @current = data.split[0]
+
+    @current = @head
+    # @count +=1
+    @remaining_data = data.split[1..-1]
+
+    @remaining_data.each do |x|
+       @current.next_node = Node.new(x)
+       @current = current.next_node
+       @count += 1
+      end
 
     end
-    # Notice the key point here -- the first piece
-    # of data we append becomes the Head, while
-    # the second becomes the Next Node of that (Head) node.
+   def prepend(data)
+     if @head == nil
+       head_create(data)
+     else
+       add_node_not_head(data)
+     end
+     temp = @head
+     append(data)
+     @current.next_node = temp
 
-  end
+   end
+
   def count
     @count
   end
-  def to_string(node)
-    if @count == 1
-    node.data
-    else
-  node.data + " " + node.next_node.data
-    end
+  def to_string
+    string = []
+    @current = @head
+
+    while @current.next_node != nil
+    string << @current.data
+    @current = @current.next_node
+     end
+    # retrieve data from @data for each node
+    string << @current.data
+    string.join(' ')
   end
 
 end
--- test/jungle_beat_test.rb
+++ test/jungle_beat_test.rb
@@ -23,44 +23,66 @@ class JungleTest < Minitest::Test
   end
 
   def test_append_sound
-
       list = LinkedList.new
-      node = Node.new("doop")
-      list.append(node)
-
+      data = "doop"
+      list.append(data)
       assert_equal "doop", list.head.data
   end
+
   def test_list_head_next_node_is_nil
     list = LinkedList.new
-    node = Node.new("doop")
-    list.append(node)
+    data = "doop"
+    list.append(data)
     assert_nil list.head.next_node
   end
+
   def test_list_count
+
     list = LinkedList.new
-    node = Node.new("doop")
-    list.append(node)
+    data = "doop"
+    list.append(data)
     list.count
 
     assert_equal 1 , list.count
   end
   def test_list_to_string
-    list = LinkedList.new
-    node = Node.new("doop")
-    list.append(node)
-    list.to_string(node)
 
-    assert_equal "doop", list.to_string(node)
+    list = LinkedList.new
+    data = "doop"
+    list.append(data)
+    list.to_string
+
+    assert_equal "doop", list.to_string
  end
 
  def test_list_append_multiple
-   list = LinkedList.new
-   node = Node.new("doop")
-   list.append(node)
-   list.append(node)
 
-   binding.pry
-   assert_equal "doop deep", list.to_string(node)
+   list = LinkedList.new
+   data = "doop deep"
+
+  #  node = Node.new("doop")
+  #  next_node = Node.new("deep")
+   list.append(data)
+  #  list.append(next_node)
+
+
+   assert_equal "doop deep", list.to_string
+ end
+
+ def test_list_prepend
+   list = LinkedList.new
+   list.head_create("dang")
+   data = "dimp dumb dog blam"
+   list.prepend(data)
+
+  assert_equal "dimp dumb dog blam dang", list.to_string
+ end
+
+ def test_shit
+   list = LinkedList.new
+   list.append("dunk")
+    binding.pry
+    puts "----"
  end
 
 end
