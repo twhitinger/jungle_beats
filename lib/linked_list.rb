94af8dd81ccd042e14d7e73962f556ec1850739b
Trent Whitinger <twhitinger@gmail.com>
2016-05-17 22:48:42 -0600

    finishing up first round of completed tests

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -4,87 +4,103 @@ class LinkedList
   attr_reader :head, :remaining_data, :current
   def initialize(head = nil)
     @head = head
-    @count = 0
     @current = @head
-
   end
+
   def head_create(data)
     data = data.split[0]
     node = Node.new(data)
     @head = node
-    @count += 1
   end
+
   def add_node_not_head(data)
     data = data.split[0]
-
     # go to the last node and add it to next node
     @current = @head
     until @current.next_node == nil
       @current = @current.next_node
     end
-
     @current.next_node = Node.new(data)
   end
 
   def append(data)
-
-   if @head == nil
-       head_create(data)
-   else
-       add_node_not_head(data)
-       @current = @current.next_node
-   end
-  #  add_node_not_head(data)
-
-
-    # @current = data.split[0]
-
-
-    # @count +=1
-    @remaining_data = data.split[1..-1]
-
-    @remaining_data.each do |x|
-       @current.next_node = Node.new(x)
-       @current = current.next_node
-       @count += 1
-      end
-
+    if @head == nil
+      head_create(data)
+      @current = @head
+    else
+      add_node_not_head(data)
+      @current = @current.next_node
     end
-   def prepend(data)
 
-     temp = @head
-     head_create(data)
-     @current = @head
-     @remaining_data = data.split[1..-1]
-     @remaining_data.each do |x|
-        @current.next_node = Node.new(x)
-        @current = current.next_node
-        @count += 1
-       end
+    remaining_data = data.split[1..-1]
+    remaining_data.each do |x|
+      @current.next_node = Node.new(x)
+      @current = current.next_node
+    end
+  end
 
-     old_data = to_string
-     @current = @head
-     @remaining_data = old_data.split[1..-1]
-     @remaining_data.each do |x|
-        @current.next_node = Node.new(x)
-        @current = current.next_node
-        @count += 1
-       end
 
-       @current.next_node = temp
-   end
+  def prepend(data)
+    temp = @head
+    head_create(data)
+    @current = @head
+    remaining_data = data.split[1..-1]
+    remaining_data.each do |x|
+      @current.next_node = Node.new(x)
+      @current = current.next_node
+    end
+
+    old_data = to_string
+    @current = @head
+    remaining_data = old_data.split[1..-1]
+    remaining_data.each do |x|
+      @current.next_node = Node.new(x)
+      @current = current.next_node
+    end
+    @current.next_node = temp
+  end
+
+  def insert(index, data)
+    new_data = to_string.split.insert(index, data).join(' ')
+    @head = nil
+    prepend(new_data)
+  end
+
+  def find(index,addition)
+    if addition < 0
+    end_range = 0
+    puts "negative words can't be returned"
+    else
+    addition == 1 ? end_range = index : end_range = index + addition - 1
+    end
+    to_string.split[index..end_range].join(' ')
+  end
+
+  def includes?(str)
+    to_string.split.include?(str)
+  end
+
+  def pop
+    last_item = to_string.split.pop
+    new_data = to_string.split
+    new_data.delete(last_item)
+    @head = nil
+    append(new_data.join(' '))
+    last_item
+  end
+
   def count
     to_string.split.count
   end
+
   def to_string
     string = []
     @current = @head
-
     while @current.next_node != nil
-    string << @current.data
-    @current = @current.next_node
-     end
-    # retrieve data from @data for each node
+      string << @current.data
+      @current = @current.next_node
+    end
+
     string << @current.data
     string.join(' ')
   end
--- test/jungle_beat_test.rb
+++ test/jungle_beat_test.rb
@@ -8,29 +8,29 @@ require_relative '../lib/node'
 class JungleTest < Minitest::Test
 
   def test_node_data_equals_plop
-      skip
-      node = Node.new("plop")
-      assert_equal "plop", node.data
+    skip
+    node = Node.new("plop")
+    assert_equal "plop", node.data
   end
 
   def test_next_node_nil_for_last_node
-     skip
-      node = Node.new("plop")
-      assert_nil node.next_node
+    skip
+    node = Node.new("plop")
+    assert_nil node.next_node
   end
 
   def test_node_head_nil
-      skip
-      list = LinkedList.new
-      assert_nil list.head
+    skip
+    list = LinkedList.new
+    assert_nil list.head
   end
 
   def test_append_sound
-      skip
-      list = LinkedList.new
-      data = "doop"
-      list.append(data)
-      assert_equal "doop", list.head.data
+    skip
+    list = LinkedList.new
+    data = "doop"
+    list.append(data)
+    assert_equal "doop", list.head.data
   end
 
   def test_list_head_next_node_is_nil
@@ -50,45 +50,81 @@ class JungleTest < Minitest::Test
 
     assert_equal 1 , list.count
   end
+
   def test_list_to_string
-      skip
+    skip
     list = LinkedList.new
     data = "doop"
     list.append(data)
     list.to_string
-
     assert_equal "doop", list.to_string
- end
+  end
 
- def test_list_append_multiple
-   skip
-   list = LinkedList.new
-   data = "doop deep"
+  def test_list_append_multiple
+    skip
+    list = LinkedList.new
+    data = "doop deep"
+    list.append(data)
+    assert_equal "doop deep", list.to_string
+  end
 
-  #  node = Node.new("doop")
-  #  next_node = Node.new("deep")
-   list.append(data)
-  #  list.append(next_node)
+  def test_list_prepend
+    skip
+    list = LinkedList.new
+    list.head_create("dang")
+    data = "dimp dumb dog blam"
+    list.prepend(data)
+    assert_equal "dimp dumb dog blam dang", list.to_string
+  end
 
-
-   assert_equal "doop deep", list.to_string
- end
-
- def test_list_prepend
-   skip
-   list = LinkedList.new
-   list.head_create("dang")
-   data = "dimp dumb dog blam"
-   list.prepend(data)
-
-  assert_equal "dimp dumb dog blam dang", list.to_string
- end
-
- def test_shit
-   list = LinkedList.new
-   list.append("dunk")
+  def test_shit
+    skip
+    list = LinkedList.new
+    list.append("dunk the monkey")
     binding.pry
     puts "----"
- end
+  end
 
+  def test_list_append_convert_to_string
+    skip
+    list = LinkedList.new
+    list.append("plop")
+    list.to_string
+    assert_equal "plop", list.head.data
+    assert_equal "plop", list.to_string
+  end
+
+  def test_to_string_with_count
+    skip
+    list = LinkedList.new
+    list.append("plop")
+    list.append("suu")
+    list.prepend("dop")
+    list.to_string
+    list.count
+    assert_equal "dop plop suu", list.to_string
+    assert_equal 3, list.count
+  end
+
+  def test_insert_to_string
+    skip
+    list = LinkedList.new
+    list.append("plop")
+    list.append("suu")
+    list.prepend("dop")
+    list.insert(1, "woo")
+    list.to_string
+    assert_equal "dop woo plop suu", list.to_string
+  end
+
+  def test_find
+    skip
+    list = LinkedList.new
+    list.append("deep woo shi shu blop")
+    list.find(2,1)
+    list.find(1,3)
+    binding.pry
+    assert_equal "shi", list.find(2,1)
+    assert_equal "woo shi shu", list.find(1,3)
+  end
 end
